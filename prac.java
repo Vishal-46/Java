e6e8ecd14875b5f7e32e088d8e23005b6c5dd781
Vishal-46 <vishalmurugan006@gmail.com>
2025-10-07 10:29:46 +0530

    Java Env setting and testing through a simple class

---
+++ prac.java
@@ -0,0 +1,17 @@
+public class prac {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Variables
+        int number = 42;
+        String message = "Java is working!";
+        
+        // Simple calculation
+        int result = number * 2;
+        
+        // Output
+        System.out.println("Number: " + number);
+        System.out.println("Message: " + message);
+        System.out.println("Result: " + result);
+    }
+}
